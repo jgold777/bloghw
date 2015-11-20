f23659b656c2134ca052b3671b086edfd7d53965
Jason S. Gold <jason.gold@careerbuilder.com>
2015-11-19 22:16:11 -0500

    generated friendly_id but thats it - next need to generate migration

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :comments
   resources :posts
-  root to: "categories#index"
+  root to: "posts#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
